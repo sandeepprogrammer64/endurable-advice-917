eea2c3b3d9d3bfdb99e25433735a07be88de7c7f
Sandeep Bhai <mail4sandeep44@gmail.com>
2022-11-09 18:25:50 +0530

    doing

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,12 +1,20 @@
 
-import { Box, Flex, Image } from "@chakra-ui/react"
+import { Box, Button, Flex, Image, Select, Stack } from "@chakra-ui/react"
 
 function Navbar()
 {
     return (
         <Flex bg="red.300">
-        <Box boxSize='100px' p="20px" marginLeft="22px">
+        <Box width='100px' p="20px" marginLeft="22px" display="flex">
            <Image src='https://assets.pharmeasy.in/apothecary/images/logo_big.svg?dim=256x0'  alt='PharmEasy' />
+          Delivery To
+         <Select placeholder="Select Pincode">
+            
+         </Select>
+           
+          
+           
+           
         </Box>
         </Flex>
 
