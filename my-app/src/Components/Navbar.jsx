b98f40d5b8bd52b94283c0c086adbe0ba59748f3
Sandeep Bhai <mail4sandeep44@gmail.com>
2022-11-10 09:39:28 +0530

    progress

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,23 +1,50 @@
 
-import { Box, Button, Flex, Image, Select, Stack } from "@chakra-ui/react"
+import { Box, Button, Flex, Image, Input, Select, Stack, Container, Skeleton } from "@chakra-ui/react"
+import { useState } from "react";
 
 function Navbar()
 {
+
+    const [loading, setloading] = useState(true);
+
+    // if(loading)
+    // {
+    //     return( 
+    //     <Stack>
+    //     <Skeleton height='20px' />
+    //     <Skeleton height='20px' />
+    //     <Skeleton height='20px' />
+    //   </Stack>
+    //     )
+    // }
+   
+
+   
     return (
-        <Flex bg="red.300">
-        <Box width='100px' p="20px" marginLeft="22px" display="flex">
-           <Image src='https://assets.pharmeasy.in/apothecary/images/logo_big.svg?dim=256x0'  alt='PharmEasy' />
+
+       
+
+        <Container >
+        <Flex >
+        <Box  display="flex">
+           <Image  src='https://assets.pharmeasy.in/apothecary/images/logo_big.svg?dim=256x0'  alt='PharmEasy' />
           Delivery To
          <Select placeholder="Select Pincode">
             
          </Select>
+
+         <Button>Download App</Button>
            
           
            
            
         </Box>
-        </Flex>
 
+     </Flex>
+
+     </Container>
+
+        
     
     )
 }
