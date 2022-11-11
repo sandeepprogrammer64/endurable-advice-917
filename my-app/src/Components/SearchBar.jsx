63e4ff5d7acdb1edbb6988b2900c12184e5a45e6
Sandeep Bhai <mail4sandeep44@gmail.com>
2022-11-11 09:24:49 +0530

    progress

--- my-app/src/Components/SearchBar.jsx
+++ my-app/src/Components/SearchBar.jsx
@@ -1,9 +1,17 @@
-import {Input} from "@chakra-ui/react"
+import {Container, Input, Text, Button, Flex} from "@chakra-ui/react";
+import { Icon, createIcon } from '@chakra-ui/icons'
 
 function SearchBar()
 {
     return (
-        <Input size="md" width="60%" borderRadius="10px" marginTop="20px"/>
+        <Container w="100%">
+            <b>What are You Looking For?</b>
+        <Flex>
+        <Input size="lg" width="130%" borderRadius="50px" marginTop="20px"/>
+        <Button colorScheme="teal">Search</Button>
+        </Flex>
+
+        </Container>
     )
 }
 export default SearchBar;
