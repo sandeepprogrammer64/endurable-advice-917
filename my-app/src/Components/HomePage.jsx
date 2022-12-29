249cbdb718232661c5567236a4b9073fd5380c94
Sandeep Bhai <mail4sandeep44@gmail.com>
2022-12-29 13:06:27 +0530

    ...

--- my-app/src/Components/HomePage.jsx
+++ my-app/src/Components/HomePage.jsx
@@ -118,12 +118,14 @@ function HomePage() {
           />
           <Text p="10px" fontWeight="bold">
             Health Blogs
+
+            
           </Text>
         </Box>
 
         
 
-        
+
 
         <Box className="boxes" border="1px solid gray.200">
           <Image
