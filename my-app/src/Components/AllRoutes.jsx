0f12b0587b206062e9b236dab1030d729ba4021d
Sandeep Bhai <mail4sandeep44@gmail.com>
2023-01-04 07:36:13 +0530

    update

--- my-app/src/Components/AllRoutes.jsx
+++ my-app/src/Components/AllRoutes.jsx
@@ -1,23 +1,14 @@
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Download from "./Download";
 
-
-
-
-
-function AllRoutes()
-{
-    return (
-        <Routes>
-            <Route path="/" element={<Navbar />}/>
-            <Route path="/download" element={<Download />}  />
-        </Routes>
-
-
-    )
-
-    
+function AllRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />} />
+      <Route path="/download" element={<Download />} />
+    </Routes>
+  );
 }
 
 export default AllRoutes;
